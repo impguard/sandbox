bf4c4cb242fe9488c729fef17f0464ef12e159a5
kwu <kwu@riotgames.com>
2017-12-02 22:15:44 -0800

    Implemented cooler thin in io and reworked Bakefile.

--- books/learn-you-a-haskell/io.hs
+++ books/learn-you-a-haskell/io.hs
@@ -4,11 +4,14 @@
 --     putStrLn $ "Hey " ++ name ++ ", you rock!"
 
 
-main = do
-    string <- getLine
-    if null string
-        then return ()
-        else do
-            putStrLn $ reverse string
-            main
+-- main = do
+--     string <- getLine
+--     if null string
+--         then return ()
+--         else do
+--             putStrLn $ reverse string
+--             main
+--
 
+main = do
+    interact $ unlines . map (unwords . map reverse . words) . lines
