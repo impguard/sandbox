f38afcc71d353da7c1cfceaafbc0390b4fa1e4e3
kwu <kwu@riotgames.com>
2018-01-04 10:32:58 +0800

    Added some more files.

---
+++ books/haskell-book/ch13/exe/Main.hs
@@ -0,0 +1,13 @@
+module Main
+  ( main
+  ) where
+
+import           Hello
+import           System.IO
+
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering
+  putStr "Please input your name: "
+  name <- getLine
+  sayHello name
