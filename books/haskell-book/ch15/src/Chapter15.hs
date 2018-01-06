fd0c82f5ca65c6411591e7ac7750bbca07c29900
kwu <kwu@riotgames.com>
2018-01-06 03:22:22 -0800

    Did more Chapter 15 exercises.

--- books/haskell-book/ch15/src/Chapter15.hs
+++ books/haskell-book/ch15/src/Chapter15.hs
@@ -3,6 +3,10 @@ module Chapter15
   , First'
   , Trivial
   , Two
+  , Three
+  , BoolConj
+  , Combine(unCombine)
+  , BoolDisj
   ) where
 
 import           Data.Semigroup
@@ -31,13 +35,13 @@ instance Monoid (First' a) where
   mappend a (First' Nada) = a
   mappend a _             = a
 
-first'Gen :: Arbitrary a => Gen (First' a)
-first'Gen = do
+firstGen :: Arbitrary a => Gen (First' a)
+firstGen = do
   a <- arbitrary
   return $ First' (Only a)
 
 instance Arbitrary a => Arbitrary (First' a) where
-  arbitrary = frequency [(1, return $ First' Nada), (3, first'Gen)]
+  arbitrary = frequency [(1, return $ First' Nada), (3, firstGen)]
 
 -- Semigroup Exercises
 -- 1.
@@ -53,7 +57,8 @@ instance Arbitrary Trivial where
 
 -- 3.
 data Two a b =
-  Two a b
+  Two a
+      b
   deriving (Eq, Show)
 
 instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
@@ -64,3 +69,70 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
     a <- arbitrary
     b <- arbitrary
     return $ Two a b
+
+-- 4.
+data Three a b c =
+  Three a
+        b
+        c
+  deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c) =>
+         Semigroup (Three a b c) where
+  (<>) (Three a b c) (Three d e f) = Three (a <> d) (b <> e) (c <> f)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) =>
+         Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return $ Three a b c
+
+-- 6.
+newtype BoolConj =
+  BoolConj Bool
+  deriving (Show, Eq)
+
+instance Semigroup BoolConj where
+  (<>) (BoolConj True) (BoolConj True) = BoolConj True
+  (<>) _ _                             = BoolConj False
+
+instance Arbitrary BoolConj where
+  arbitrary = BoolConj <$> arbitrary
+
+-- 9.
+newtype Combine a b = Combine
+  { unCombine :: a -> b
+  }
+
+instance Semigroup b => Semigroup (Combine a b) where
+  (<>) (Combine f) (Combine g) = Combine $ \x -> f x <> g x
+
+instance (CoArbitrary a, Arbitrary b) => Arbitrary (Combine a b) where
+  arbitrary = Combine <$> arbitrary
+
+-- Monoid Exercises
+-- 5.
+
+newtype BoolDisj = BoolDisj Bool deriving (Show, Eq)
+
+instance Monoid BoolDisj where
+  mempty = BoolDisj False
+  mappend (BoolDisj True) (BoolDisj True) = BoolDisj False
+  mappend (BoolDisj False) (BoolDisj False) = BoolDisj False
+  mappend _ _ = BoolDisj True
+
+
+instance Arbitrary BoolDisj where
+  arbitrary = BoolDisj <$> arbitrary
+
+-- 8.
+
+newtype Mem s a = Mem
+  { runMem :: s -> (a, s)
+  }
+
+instance Monoid a => Monoid (Mem s a) where
+  mempty = Mem $ \s -> (mempty, s)
+  mappend (Mem f) (Mem g) = Mem $ \s -> g s , f s
--- books/haskell-book/ch15/tests/Main.hs
+++ books/haskell-book/ch15/tests/Main.hs
@@ -19,13 +19,30 @@ prop_monoidRightIdentity a = a <> mempty == a
 prop_semigroupAssoc :: (Eq m, S.Semigroup m) => m -> m -> m -> Bool
 prop_semigroupAssoc a b c = a S.<> (b S.<> c) == (a S.<> b) S.<> c
 
+prop_combineAssoc :: (S.Semigroup b, Eq b) => Combine a b -> Combine a b -> Combine a b -> a -> Bool
+prop_combineAssoc x y z v =
+  unCombine (x S.<> y S.<> z) v == unCombine ((x S.<> y) S.<> z) v
+
 type MonoidAssoc = First' String -> First' String -> First' String -> Bool
 
 type MonoidId = First' String -> Bool
 
 type TrivialAssoc = Trivial -> Trivial -> Trivial -> Bool
 
-type TwoAssoc = Two String String -> Two String String -> Two String String -> Bool
+type TwoAssoc
+   = Two String String -> Two String String -> Two String String -> Bool
+
+type ThreeAssoc
+   = Three String String String -> Three String String String -> Three String String String -> Bool
+
+type BoolConjAssoc = BoolConj -> BoolConj -> BoolConj -> Bool
+
+type CombineAssoc
+   = Combine String String -> Combine String String -> Combine String String -> String -> Bool
+
+type BoolDisjAssoc = BoolDisj -> BoolDisj -> BoolDisj -> Bool
+
+type BoolDisjId = BoolDisj -> Bool
 
 main :: IO ()
 main
@@ -42,3 +59,11 @@ main
   quickCheck (prop_semigroupAssoc :: TrivialAssoc)
   -- Semigroup Two
   quickCheck (prop_semigroupAssoc :: TwoAssoc)
+  -- Semigroup Three
+  quickCheck (prop_semigroupAssoc :: ThreeAssoc)
+  -- Semigroup BoolConj
+  quickCheck (prop_semigroupAssoc :: BoolConjAssoc)
+  -- Monoid BoolDisj
+  quickCheck (prop_monoidAssoc :: BoolDisjAssoc)
+  quickCheck (prop_monoidLeftIdentity :: BoolDisjId)
+  quickCheck (prop_monoidRightIdentity :: BoolDisjId)
