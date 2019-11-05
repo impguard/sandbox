8d654b9018ef6433609330f59df1ca8dfe2a300c
Kevin Wu <contact@kevinwu.io>
2019-11-05 02:33:54 -0800

    Added comment on intermediate 1

--- exercises/go/intermediate1.go
+++ exercises/go/intermediate1.go
@@ -1,5 +1,11 @@
 package main
 
+//
+// Write a program that outputs all possibilities to put + or - or nothing
+// between the numbers 1,2,…,9 (in this order) such that the result is 100. For
+// example 1 + 2 + 3 - 4 + 5 + 6 + 78 + 9 = 100.
+//
+
 import (
 	"fmt"
 	"os"
